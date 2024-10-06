377eda24a7850069660de462453514a99102ace7
Juan Manuel <j-grafi@hotmail.com>
2024-10-06 23:39:39 +0200

    interation 2

--- src/memory.js
+++ src/memory.js
@@ -1,14 +1,25 @@
 class MemoryGame {
-  constructor(cards, pickedCards, pairsClicked, pairsGuessed) {
+  constructor(cards) {
     this.cards = cards;
-    this.pickedCards = pickedCards;
-    this.pairsClicked = pairsClicked;
-    this.pairsGuessed = pairsGuessed;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
     // ... write your code here
+    if (!this.cards) {
+      return undefined
+    }
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+      
+    }
+    return this.cards;
+
+
   }
 
   checkIfPair(card1, card2) {
